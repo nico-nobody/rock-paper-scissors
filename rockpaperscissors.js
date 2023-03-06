eebb5673264cea0a984fe373d21e1bc86f89874e
VinylSlug <nicholasgutierrez95@gmail.com>
2023-03-05 16:41:51 -0800

    add getComputerChoice()

--- rockpaperscissors.js
+++ rockpaperscissors.js
@@ -1,6 +1,16 @@
 // Rock Paper Scissors against Computer
 
 // 1: Get random choice for computer
+function getComputerChoice() {
+  switch (Math.floor(Math.random() * 3)) {
+    case 0:
+      return 'rock';
+    case 1:
+      return 'paper';
+    case 2:
+      return 'scissors';
+  }
+}
 // 2: Prompt user for their choice
 // 3: Compare choices and decide winner
 // 4: Display winner and results
