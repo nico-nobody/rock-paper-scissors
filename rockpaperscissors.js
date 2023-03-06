cc11fdf799c3c010758b352524071247fe30504a
VinylSlug <nicholasgutierrez95@gmail.com>
2023-03-05 16:40:45 -0800

    Initialize .html and .js

---
+++ rockpaperscissors.js
@@ -0,0 +1,6 @@
+// Rock Paper Scissors against Computer
+
+// 1: Get random choice for computer
+// 2: Prompt user for their choice
+// 3: Compare choices and decide winner
+// 4: Display winner and results
