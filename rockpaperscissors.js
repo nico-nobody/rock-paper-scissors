3ca498b72c8f5dd0128deffa2568f2ef5829361a
VinylSlug <nicholasgutierrez95@gmail.com>
2023-03-14 17:14:38 -0700

    Recoded game for UI controls and initiated CSS

--- rockpaperscissors.js
+++ rockpaperscissors.js
@@ -1,6 +1,40 @@
 // Rock Paper Scissors against Computer
 
-// 1: Get random choice for computer
+// 1: Get playerSelection from their button choice and initiate round.
+let playerChoice;
+const buttons = document.querySelectorAll('#weapons-container > button')
+// console.log(buttons)
+buttons.forEach((button) => {
+  button.addEventListener('click', () => {
+    playerChoice = button.id;
+  })
+})
+const playBtn = document.querySelector('#play-rnd')
+playBtn.addEventListener('click', () => {
+  playRound(playerChoice);
+})
+
+// 2: Begin round with initializing computerSelection then compare selections, returning winner as outcome.
+// Rock > Scissors
+// Paper > Rock
+// Scissors > Paper
+function playRound(playerSelection) {
+  let outcome;
+  let computerSelection = getComputerChoice();
+  if (playerSelection === computerSelection) {
+    outcome = 'draw';
+  } else if (playerSelection === 'rock') {
+    outcome = computerSelection === 'scissors' ? 'player' : 'computer';
+  } else if (playerSelection === 'paper') {
+    outcome = computerSelection === 'rock' ? 'player' : 'computer';
+  } else if (playerSelection === 'scissors') {
+    outcome = computerSelection === 'paper' ? 'player' : 'computer';
+  }
+  // console.log(playerSelection ,computerSelection, outcome);
+  const results = document.querySelector('#results');
+  results.innerText = `Player: ${playerSelection}, Computer: ${computerSelection}\nRound: ${outcome}`;
+  updateTotals(outcome);
+}
 function getComputerChoice() {
   switch (Math.floor(Math.random() * 3)) {
     case 0:
@@ -12,47 +46,29 @@ function getComputerChoice() {
   }
 }
 
-// 2: Prompt user for their choice
-function getPlayerChoice() {
-  let choice;
-  do {
-    let input = prompt('Choose: Rock, Paper, or Scissors?');
-    choice = input.toLowerCase();
-  } while (choice !== 'rock' && choice !== 'paper' && choice !== 'scissors');
-  return choice;
+// 3: Add to each players win total.
+// Over when first player to 5 points
+let playerTotal = 0;
+let computerTotal = 0;
+function updateTotals(winner) {
+  if (winner === 'player') playerTotal++;
+  else if (winner === 'computer') computerTotal++;
+  const totals = document.querySelector('#total-wins');
+  totals.innerText = `Total wins:\nPlayer: ${playerTotal}, Computer: ${computerTotal}`;
 }
 
-// 3: Compare choices and display winner
-// Rock > Scissors
-// Paper > Rock
-// Scissors > Paper
-function playRound(playerSelection, computerSelection) {
-  let outcome;
-  
-  if (playerSelection === computerSelection) {
-    outcome = 'draw';
-  } else if (playerSelection === 'rock') {
-    outcome = computerSelection === 'scissors' ? 'player' : 'computer';
-  } else if (playerSelection === 'paper') {
-    outcome = computerSelection === 'rock' ? 'player' : 'computer';
-  } else if (playerSelection === 'scissors') {
-    outcome = computerSelection === 'paper' ? 'player' : 'computer';
-  }
-
-  return outcome;
-}
 
 //4: Play a game comprised of 5 rounds
-function game() {
-  let playerPoints = 0;
-  let computerPoints = 0;
-  for (let i = 0; i < 5; i++) {
-    let computerSelection = getComputerChoice();
-    let playerSelection = getPlayerChoice();
-    let winner = playRound(playerSelection, computerSelection);
-    if (winner === 'player') playerPoints++;
-    else if (winner === 'computer') computerPoints++;
-    console.log(`Player: ${playerSelection}, Computer: ${computerSelection}`);
-  }
-  return `Game. Player: ${playerPoints}, Computer: ${computerPoints}`;
-}
+// function game() {
+//   let playerPoints = 0;
+//   let computerPoints = 0;
+//   for (let i = 0; i < 5; i++) {
+//     let computerSelection = getComputerChoice();
+//     let playerSelection = getPlayerChoice();
+//     let winner = playRound(playerSelection, computerSelection);
+//     if (winner === 'player') playerPoints++;
+//     else if (winner === 'computer') computerPoints++;
+//     console.log(`Player: ${playerSelection}, Computer: ${computerSelection}`);
+//   }
+//   return `Game. Player: ${playerPoints}, Computer: ${computerPoints}`;
+// }
