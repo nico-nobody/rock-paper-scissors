25cc8f11301d16aae9b53ad478f0c5a01fa8dc2e
VinylSlug <nicholasgutierrez95@gmail.com>
2023-03-14 21:59:49 -0700

    Update UI

--- rockpaperscissors.js
+++ rockpaperscissors.js
@@ -5,12 +5,14 @@ let playerChoice;
 const buttons = document.querySelectorAll('#weapons-container > button')
 buttons.forEach((button) => {
   button.addEventListener('click', () => {
+    if(playerChoice !== undefined) document.querySelector(`#${playerChoice}`).classList.remove('selected');
     playerChoice = button.id;
+    button.classList.add('selected');
   })
 })
 const playBtn = document.querySelector('#play-rnd')
 playBtn.addEventListener('click', () => {
-  playRound(playerChoice);
+  if(playerChoice !== undefined) playRound(playerChoice);
 })
 
 // 2: Begin round with initializing computerSelection then compare selections, returning winner as outcome.
@@ -20,13 +22,13 @@ playBtn.addEventListener('click', () => {
 function playRound(playerSelection) {
   let outcome;
   let computerSelection = getComputerChoice();
-  if (playerSelection === computerSelection) {
+  if(playerSelection === computerSelection) {
     outcome = 'draw';
-  } else if (playerSelection === 'rock') {
+  }else if(playerSelection === 'rock') {
     outcome = computerSelection === 'scissors' ? 'player' : 'computer';
-  } else if (playerSelection === 'paper') {
+  }else if(playerSelection === 'paper') {
     outcome = computerSelection === 'rock' ? 'player' : 'computer';
-  } else if (playerSelection === 'scissors') {
+  }else if(playerSelection === 'scissors') {
     outcome = computerSelection === 'paper' ? 'player' : 'computer';
   }
   const results = document.querySelector('#results');
@@ -34,7 +36,7 @@ function playRound(playerSelection) {
   updateTotals(outcome);
 }
 function getComputerChoice() {
-  switch (Math.floor(Math.random() * 3)) {
+  switch(Math.floor(Math.random() * 3)) {
     case 0:
       return 'rock';
     case 1:
@@ -49,8 +51,8 @@ function getComputerChoice() {
 let playerTotal = 0;
 let computerTotal = 0;
 function updateTotals(winner) {
-  if (winner === 'player') playerTotal++;
-  else if (winner === 'computer') computerTotal++;
+  if(winner === 'player') playerTotal++;
+  else if(winner === 'computer') computerTotal++;
   const totals = document.querySelector('#total-wins');
   totals.innerText = `Total wins:\nPlayer: ${playerTotal}, Computer: ${computerTotal}`;
 }
